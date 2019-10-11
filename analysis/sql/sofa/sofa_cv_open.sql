2b39d4f6a447af16e6a19488e95f04bc1b22ef70
theonesp <ma.armengol.hcsc@gmail.com>
2019-10-11 13:04:35 -0400

    sofa_cv corrected

--- analysis/sql/sofa/sofa_cv_open.sql
+++ analysis/sql/sofa/sofa_cv_open.sql
@@ -1,6 +1,4 @@
-CREATE TABLE
-  public.sofa_cv_open AS (
-  WITH
+WITH
     t1 AS (
     WITH
       tt1 AS (
@@ -11,10 +9,9 @@ CREATE TABLE
             WHEN noninvasivemean IS NOT NULL THEN noninvasivemean
             ELSE NULL END) AS map
       FROM
-        eicu_crd.vitalaperiodic
+        `physionet-data.eicu_crd.vitalaperiodic`
       WHERE
-        observationoffset BETWEEN -1440
-        AND 1440
+        observationoffset BETWEEN -1440 AND 1440
       GROUP BY
         patientunitstayid ),
       tt2 AS (
@@ -25,10 +22,9 @@ CREATE TABLE
             WHEN systemicmean IS NOT NULL THEN systemicmean
             ELSE NULL END) AS map
       FROM
-        eicu_crd.vitalperiodic
+        `physionet-data.eicu_crd.vitalperiodic`
       WHERE
-        observationoffset BETWEEN -1440
-        AND 1440
+        observationoffset BETWEEN -1440 AND 1440
       GROUP BY
         patientunitstayid )
     SELECT
@@ -39,7 +35,7 @@ CREATE TABLE
         ELSE NULL
       END AS map
     FROM
-      eicu_crd.patient pt
+      `physionet-data.eicu_crd.patient` pt
     LEFT OUTER JOIN
       tt1
     ON
@@ -55,17 +51,16 @@ CREATE TABLE
       DISTINCT patientunitstayid,
       MAX(
         CASE
-          WHEN LOWER(drugname) LIKE '%(ml/hr)%' THEN ROUND(CAST(drugrate AS numeric)/3,3) -- rate in ml/h * 1600 mcg/ml / 80 kg / 60 min, to convert in mcg/kg/min
-          WHEN LOWER(drugname) LIKE '%(mcg/kg/min)%' THEN CAST(drugrate AS numeric)
+          WHEN LOWER(drugname) LIKE '%(ml/hr)%' THEN ROUND(CAST(drugrate AS INT64)/3,3) -- rate in ml/h * 1600 mcg/ml / 80 kg / 60 min, to convert in mcg/kg/min
+          WHEN LOWER(drugname) LIKE '%(mcg/kg/min)%' THEN CAST(drugrate AS INT64)
           ELSE NULL
         END ) AS dopa
     FROM
-      eicu_crd.infusiondrug id
+      `physionet-data.eicu_crd.infusiondrug` id
     WHERE
       LOWER(drugname) LIKE '%dopamine%'
-      AND infusionoffset BETWEEN -120
-      AND 1440
-      AND drugrate ~ '^[0-9]{0,5}$'
+      AND infusionoffset BETWEEN -120 AND 1440
+      AND REGEXP_CONTAINS(drugrate, '^[0-9]{0,5}$')
       AND drugrate<>''
       AND drugrate<>'.'
     GROUP BY
@@ -76,18 +71,18 @@ CREATE TABLE
     SELECT
       DISTINCT patientunitstayid,
       MAX(CASE
-          WHEN LOWER(drugname) LIKE '%(ml/hr)%' AND drugrate<>'' AND drugrate<>'.' THEN ROUND(CAST(drugrate AS numeric)/300,3) -- rate in ml/h * 16 mcg/ml / 80 kg / 60 min, to convert in mcg/kg/min
-          WHEN LOWER(drugname) LIKE '%(mcg/min)%' AND drugrate<>'' AND drugrate<>'.' THEN ROUND(CAST(drugrate AS numeric)/80,3)-- divide by 80 kg
-          WHEN LOWER(drugname) LIKE '%(mcg/kg/min)%' AND drugrate<>'' AND drugrate<>'.' THEN CAST(drugrate AS numeric)
+          WHEN LOWER(drugname) LIKE '%(ml/hr)%' AND drugrate<>'' AND drugrate<>'.' THEN ROUND(CAST(drugrate AS INT64)/300,3) -- rate in ml/h * 16 mcg/ml / 80 kg / 60 min, to convert in mcg/kg/min
+          WHEN LOWER(drugname) LIKE '%(mcg/min)%' AND drugrate<>'' AND drugrate<>'.' THEN ROUND(CAST(drugrate AS INT64)/80,3)-- divide by 80 kg
+          WHEN LOWER(drugname) LIKE '%(mcg/kg/min)%' AND drugrate<>'' AND drugrate<>'.' THEN CAST(drugrate AS INT64)
           ELSE NULL
         END ) AS norepi
     FROM
-      eicu_crd.infusiondrug id
+      `physionet-data.eicu_crd.infusiondrug` id
     WHERE
       LOWER(drugname) LIKE '%epinephrine%'
       AND infusionoffset BETWEEN -120
       AND 1440
-      AND drugrate ~ '^[0-9]{0,5}$'
+      AND REGEXP_CONTAINS(drugrate, '^[0-9]{0,5}$')
       AND drugrate<>''
       AND drugrate<>'.'-- this regex will capture norepi as well
     GROUP BY
@@ -99,15 +94,14 @@ CREATE TABLE
       DISTINCT patientunitstayid,
       1 AS dobu
     FROM
-      eicu_crd.infusiondrug id
+      `physionet-data.eicu_crd.infusiondrug` id
     WHERE
       LOWER(drugname) LIKE '%dobutamin%'
       AND drugrate <>''
       AND drugrate<>'.'
       AND drugrate <>'0'
-      AND drugrate ~ '^[0-9]{0,5}$'
-      AND infusionoffset BETWEEN -120
-      AND 1440
+      AND REGEXP_CONTAINS(drugrate, '^[0-9]{0,5}$')
+      AND infusionoffset BETWEEN -120 AND 1440
     ORDER BY
       patientunitstayid )
   SELECT
@@ -117,15 +111,15 @@ CREATE TABLE
     t3.norepi,
     t4.dobu,
     (CASE
-        WHEN dopa>=15 OR norepi>0.1 THEN 4
-        WHEN dopa>5
-      OR (norepi>0
+        WHEN dopa >=15 OR norepi >0.1 THEN 4
+        WHEN dopa >5
+      OR (norepi > 0
         AND norepi <=0.1) THEN 3
         WHEN dopa<=5 OR dobu > 0 THEN 2
         WHEN map <70 THEN 1
         ELSE 0 END) AS SOFA_cv
   FROM
-    public.cohort1 pt / eicu_crd.patient
+    `physionet-data.eicu_crd.patient` pt
   LEFT OUTER JOIN
     t1
   ON
@@ -143,4 +137,4 @@ CREATE TABLE
   ON
     t4.patientunitstayid=pt.patientunitstayid
   ORDER BY
-    pt.patientunitstayid );
+    pt.patientunitstayid
