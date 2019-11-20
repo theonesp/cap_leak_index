a98ba03a66b22e9bb4a541ff1bd8ab9bf8026a13
theonesp <ma.armengol.hcsc@gmail.com>
2019-11-20 15:51:33 -0500

    fixing duplication in apache vars new models trained training model workflow automatized

--- analysis/sql/apache_related.sql
+++ analysis/sql/apache_related.sql
@@ -4,6 +4,15 @@
 --        cap_leak_index, 20190511 NYU Datathon
 --        eICU Collaborative Research Database v2.0.
 -- ------------------------------------------------------------------
-SELECT patientunitstayid, apachescore, actualicumortality, 
-actualhospitalmortality, unabridgedunitlos, unabridgedhosplos, unabridgedactualventdays 
+SELECT 
+  patientunitstayid
+, MAX(apachescore) AS apachescore
+, MAX(actualicumortality) AS actualicumortality
+, MAX(actualhospitalmortality) AS actualhospitalmortality
+, MAX(unabridgedunitlos) AS unabridgedunitlos
+, MAX(unabridgedhosplos) AS unabridgedhosplos
+, MAX(unabridgedactualventdays) AS unabridgedactualventdays
+
 FROM `physionet-data.eicu_crd.apachepatientresult`
+
+GROUP BY patientunitstayid
