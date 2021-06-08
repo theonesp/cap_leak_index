64f07e96e6c9ac201e6751f842c29b707ed815a1
theonesp <ma.armengol.hcsc@gmail.com>
2021-06-08 13:52:43 +0200

    mimic iv project removal

--- mimic_iv_vli/analysis/sql/oasis_related.sql
+++
@@ -1,9 +0,0 @@
--- Extracts oasis severity score.
-
-SELECT
-  stay_id,
-  MAX(OASIS) AS oasis
-FROM
-  `physionet-data.mimic_derived.oasis`
-GROUP BY
-  stay_id
--- mimic_iv_vli/analysis/sql/rrt.sql
+++
@@ -1,3 +0,0 @@
--- Gets RRT Patients
-
-SELECT DISTINCT stay_id FROM `physionet-data.mimic_derived.rrt` where dialysis_present = 1
